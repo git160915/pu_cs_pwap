aa585298cff425b2e4e7e6caedd11ffbd6836881
Gia Truong - H <bigdata.dev00@gmail.com>
2023-06-07 13:49:19 +0000

    Updated .gitignore to include *.class and also .attach* files; Enhanced HelloWorld to take on a single arguement that is a person/user's name

--- pu_cs_pwap/Module_01/HelloWorld.java
+++ pu_cs_pwap/Module_01/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        if (args.length > 0) {
+            System.out.println("It's nice to meet you, " + args[0] + "!");
+        }
     }
 }
