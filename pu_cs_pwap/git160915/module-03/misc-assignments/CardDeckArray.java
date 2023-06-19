4e6265296790c451a79253e31a557ba0da0d69c2
Gia Truong - GNT <gntgit160915@gmail.com>
2023-06-19 12:48:27 +0000

    Updated code to print out a deck of cards

--- pu_cs_pwap/git160915/module-03/misc-assignments/CardDeckArray.java
+++ pu_cs_pwap/git160915/module-03/misc-assignments/CardDeckArray.java
@@ -1,7 +1,21 @@
 public class CardDeckArray {
     public static void main (String[] args) {
-        System.out.println("Testing githumb branch protection");
-        System.out.println("Testing from git160915");
-        System.out.println("Testing from bdgit00");
+        String[] arrsCards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] arrsSuits = {"\u2660", "\u2663", "\u2666", "\u2665"};
+        String[] arrsCardDeck = new String[52];
+
+        for (int i = 0; i < arrsSuits.length; i++) {
+            for (int j = 0; j < arrsCards.length; j++) {
+                arrsCardDeck[j+13*i] = arrsCards[j] + arrsSuits[i];
+            }
+        }
+
+        for (int i = 0; i < arrsCardDeck.length; i++) {
+            System.out.print(arrsCardDeck[i] + " ");
+
+            if ((i + 1) % 13 == 0) {
+                System.out.println();
+            }
+        }
     }
 }
