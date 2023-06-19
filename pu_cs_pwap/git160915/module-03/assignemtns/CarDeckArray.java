dab84d4c7f035f6918fc9ecaf5bd76b0ed37179e
Gia Truong - GNT <gntgit160915@gmail.com>
2023-06-19 08:54:48 +0700

    Testing branching from git160915

--- pu_cs_pwap/git160915/module-03/assignemtns/CarDeckArray.java
+++ pu_cs_pwap/git160915/module-03/assignemtns/CarDeckArray.java
@@ -1,5 +1,6 @@
 public class CardDeckArray {
     public static void main (String[] args) {
         System.out.println("Testing githumb branch protection");
+        System.out.println(Testing from git160915");
     }
 }
