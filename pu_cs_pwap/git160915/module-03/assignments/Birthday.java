448ea4333ef4353f3d16582589a695bf35d623c4
Gia Truong - H <bigdata.dev00@gmail.com>
2023-06-27 15:48:31 +0000

    Updated Birthday.java and fixed functional/logic bugs.  Fully working with 100% passed test cases in assignment submission.

--- pu_cs_pwap/git160915/module-03/assignments/Birthday.java
+++ pu_cs_pwap/git160915/module-03/assignments/Birthday.java
@@ -3,34 +3,65 @@ public class Birthday {
         int iN = Integer.parseInt(args[0]);
         int iTrials = Integer.parseInt(args[1]);
         int iNumTrialsCounter = 0;
-        int[] iListOfFirstDuplicateBirthday = new int[iN];
+        
+        // Each index represents the number of people who entered before
+        // a shared birthday is found, this list stores up to iN+1, 
+        // in the event that somehow it took the iN'th + 1 person to enter
+        // before a shared birthday occurs.  This can happen when the 
+        // number of birthdays is a low number
+        int[] iListOfFirstDuplicateBirthday = new int[iN+1];
 
-        // Stores the list of people who have entered the room.
-        // Assume it does not exceed the number iN
-        int[] iListPeopleEnteredRoom = new int[iN];
+        // Stores a boolean at each index/day
+        // false = means no one with a birthday at that index/day
+        // true = there is already someone in the room with that birthday at this index/day
+        boolean[] bListPeopleEnteredRoom;
 
-        boolean bIsDuplicateBirthday = false;
+        // false = if no one in room has a shared birthday
+        // true = upon finding first person entering the room with a shared birthday
+        boolean bSharedBirthdayExist = false;
+
+        // random birthday
+        int iRandomBirthday = 0;
+
+        // number of people who have entered thus far
+        int iNumPeopleEntered = 0;
         
         while (iNumTrialsCounter < iTrials) {
             // Init variable which will control the loop that generates 
             // people with random birthday's entering the room
-            bIsDuplicateBirthday = false;
+            bSharedBirthdayExist = false;
 
-            for (int i = 0; i < iListOfFirstDuplicateBirthday.length && !bIsDuplicateBirthday; i++) {
-                iListPeopleEnteredRoom[i] = (int) (Math.random() * iN);
+            bListPeopleEnteredRoom = new boolean[iN + 1];
 
-                for (int j = 0; j < i && !bIsDuplicateBirthday; j++) {
-                    if (iListPeopleEnteredRoom[j] == iListPeopleEnteredRoom[i]) {
-                        iListOfFirstDuplicateBirthday[i]++;
-                        
-                        // Set this to true so that the outter for loop for "i" will exit
-                        bIsDuplicateBirthday = true;
-                    }
+            // reset number of people who have entered counter
+            iNumPeopleEntered = 0;
+
+            while (!bSharedBirthdayExist) {
+                // Generate a person with random birthday to enter into the room
+                iRandomBirthday = (int) (Math.random() * iN);
+
+                // System.out.print(iRandomBirthday + "  ");
+
+                // increment number of people who have entered
+                iNumPeopleEntered++;
+
+                if (!bListPeopleEnteredRoom[iRandomBirthday]) {
+                    // if no one at this index/birthday yet, then set to true
+                    bListPeopleEnteredRoom[iRandomBirthday] = true;
+                } else {
+                    // increment the list that tracks the number of people 
+                    // entering into a room before a shared birthday appears
+                    iListOfFirstDuplicateBirthday[iNumPeopleEntered-1]++;
+
+                    // else there is already someone with this birthday so 
+                    // set this variable so that the while loop will exit
+                    bSharedBirthdayExist = true;
                 }
             }
 
             iNumTrialsCounter++;
         }
+        // System.out.println();
 
         int iRunningTotalOfDuplicate = 0;
         double dFraction = 0.0;
@@ -39,10 +70,12 @@ public class Birthday {
         for (int i = 0; i < iListOfFirstDuplicateBirthday.length && dFraction < 0.5; i++) {
             iRunningTotalOfDuplicate += iListOfFirstDuplicateBirthday[i];
 
+            // System.out.println(iRunningTotalOfDuplicate + "  " + iListOfFirstDuplicateBirthday[i]);
+
             dFraction = (double) iRunningTotalOfDuplicate/iTrials;
 
             System.out.print(i+1 + "\t");
-            System.out.print(iRunningTotalOfDuplicate + "\t");
+            System.out.print(iListOfFirstDuplicateBirthday[i] + "\t");
             System.out.print(dFraction);
             System.out.println();
         }
