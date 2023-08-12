07a2c2bec53e1e0891e1c806f45df70777ce80a3
Gia Truong - GNT <gntgit160915@gmail.com>
2023-08-12 12:43:26 +0000

    Add optimised functionality for RevesPuzzle assignment. Completed RevesPuzzle assignment.  Optimised version according to the lecture notes.

--- pu_cs_pwap/git160915/module-06/assignments/RevesPuzzle.java
+++ pu_cs_pwap/git160915/module-06/assignments/RevesPuzzle.java
@@ -2,32 +2,27 @@ public class RevesPuzzle {
 
     // move n smallest discs from one pole to another, using the temp pole
     private static void hanoi3(int k, int n, String from, String temp, String to) {
-        if (n == k) return;
+        if (n == 0) return;
 
         hanoi3(k, n-1, from, to, temp);
-        printMoveDisk(n, from, to);
+        printMoveDisk(n + k, from, to);
         hanoi3(k, n-1, temp, from, to);
     }
     
     // move n smallest discs from one pole to another, using the temp pole
-    private static void hanoi4(int k, int n, String from, String temp1, String temp2, String to) {
-        // StdOut.println("k: " + k + ", n: " + n);
-        if (n == k) {
+    private static void hanoi4(int n, String from, String temp1, String temp2, String to) {
+        int k = (int) Math.round(n + 1 - Math.sqrt(2 * n + 1));
+
+        if (n == 0) {
             return;
-        } else if (n == k + 1) {
+        } else if (n == 1) {
             printMoveDisk(n, from, to);
             return;
         }
 
-        int iN = (int) Math.round(n + 1 - Math.sqrt(2 * n + 1));
-
-        hanoi4(k, n - 2, from, temp1, to, temp2);
-        // hanoi4(k, iN - 1, from, temp2, to, temp1);
-        printMoveDisk(n - 1, from, temp1);
-        printMoveDisk(n, from, to);
-        printMoveDisk(n - 1, temp1, to);
-        // hanoi4(k, iN - 1, temp1, from, temp2, to);
-        hanoi4(k, n - 2, temp2, from, temp1, to);
+        hanoi4(k, from, temp1, to, temp2);
+        hanoi3(k, n - k, from, temp1, to);
+        hanoi4(k, temp2, from, temp1, to);
 
         return;
     }
@@ -41,6 +36,6 @@ public class RevesPuzzle {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
 
-        hanoi4(0, n, "A", "B", "C", "D");
+        hanoi4(n, "A", "B", "C", "D");
     }
 }
